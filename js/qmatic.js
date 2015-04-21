f174618e8a6e20a2d18952052feb4b758081d48d
Jonathan Martinez <jamm@jmartinezm.org>
2015-04-20 22:16:14 -0430

    Agreagados el visualizar el numero de clientes en espera y la lista de tickets al operador

--- js/qmatic.js
+++ js/qmatic.js
@@ -13,37 +13,38 @@ function new_ticket(queu){
       tqnumber += 1
       taquilla_queu.push(tqnumber)
       people = taquilla_queu.length
-      wait_time = taquilla_queu.length * 3
-      // response = 'Su numero de ticket es ' + 'T-' + tqnumber +
-      //             '\nHay ' + people + ' personas delante de usted' +
-      //             '\nEl tiempo de espera aproximado es de ' + wait_time + ' minutos';
-      // console.log(response)
+      wait_time = (taquilla_queu.length * 3) - 3
       document.getElementById("newticket").innerHTML = tqnumber;
       document.getElementById("people").innerHTML = people;
       document.getElementById("waitingtime").innerHTML = wait_time;
-
-      console.log(taquilla_queu)
       break;
     case 2:
       ttqnumber += 1
       taquilla_tercera_queu.push(ttqnumber)
-      wait_time = taquilla_tercera_queu.length * 5
-      // console.log('Su numero de ticket es ' + 'TE-' + ttqnumber)
-      console.log(taquilla_tercera_queu)
+      people = taquilla_tercera_queu.length
+      wait_time = (taquilla_tercera_queu.length * 5) - 5
+      document.getElementById("newticket").innerHTML = ttqnumber;
+      document.getElementById("people").innerHTML = people;
+      document.getElementById("waitingtime").innerHTML = wait_time;
       break;
     case 3:
       acqnumber += 1
       atencion_cliente_queu.push(acqnumber)
-      wait_time = atencion_cliente_queu.length * 8
-      // console.log('Su numero de ticket es ' + 'AC-' + acqnumber)
+      wait_time = (atencion_cliente_queu.length * 8) - 8
+      people = pop_atencion_client.length
       console.log(atencion_cliente_queu)
+      document.getElementById("newticket").innerHTML = acqnumber;
+      document.getElementById("people").innerHTML = people;
+      document.getElementById("waitingtime").innerHTML = wait_time;
       break;
     case 4:
       actqnumber += 1
       atencion_cliente_tercera_queu.push(actqnumber)
-      wait_time = atencion_cliente_tercera_queu.length * 12
-      // console.log('Su numero de ticket es ' + 'ACE-' + actqnumber)
-      console.log(atencion_cliente_tercera_queu)
+      wait_time = (atencion_cliente_tercera_queu.length * 12) - 12
+      people = atencion_cliente_tercera_queu.length
+      document.getElementById("newticket").innerHTML = actqnumber;
+      document.getElementById("people").innerHTML = people;
+      document.getElementById("waitingtime").innerHTML = wait_time;
       break;
     default:
       break;
@@ -53,28 +54,28 @@ function new_ticket(queu){
 
 function pop_taquilla_client(){
   taquilla_queu.shift()
-  // console.log('Cliente atendido')
-  console.log(taquilla_queu)
+  document.getElementById("toattend1").innerHTML = taquilla_queu.length;
+  document.getElementById("ticketlist1").innerHTML = taquilla_queu;
 
 }
 
 function pop_taquilla_tercera_client(){
   taquilla_tercera_queu.shift()
-  // console.log('Cliente atendido')
-  console.log(taquilla_tercera_queu)
+  document.getElementById("toattend2").innerHTML = taquilla_tercera_queu.length;
+  document.getElementById("ticketlist2").innerHTML = taquilla_tercera_queu;
 }
 
 
 function pop_atencion_client() {
   atencion_cliente_queu.shift()
-  // console.log('Cliente atendido')
-  console.log(atencion_cliente_queu)
+  document.getElementById("toattend3").innerHTML = atencion_cliente_queu.length;
+  document.getElementById("ticketlist3").innerHTML = atencion_cliente_queu;
 }
 
 function pop_atencion_tercera_client(){
   atencion_cliente_tercera_queu.shift()
-  // console.log('Cliente atendido')
-  console.log(atencion_tercera_client)
+  document.getElementById("toattend4").innerHTML = atencion_cliente_tercera_queu.length;
+  document.getElementById("ticketlist4").innerHTML = atencion_cliente_tercera_queu;
 
 }
 
