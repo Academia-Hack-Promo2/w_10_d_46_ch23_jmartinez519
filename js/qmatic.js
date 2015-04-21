fcd9da8dbce041cba986c2bb540269d92f45c7b1
Jonathan Martinez <jamm@jmartinezm.org>
2015-04-21 11:02:58 -0430

    Corregido error para que mueste el mismo numero de ticket en el operador y cliente

--- js/qmatic.js
+++ js/qmatic.js
@@ -10,77 +10,78 @@ var atencion_cliente_tercera_queu = [];
 function new_ticket(queu){
   switch (queu) {
     case 1:
-      if (tqnumber === 299){
-        tqnumber = 100
-      }
-      else{
-        tqnumber += 1
-        taquilla_queu.push(tqnumber)
-        people = taquilla_queu.length
-        wait_time = (taquilla_queu.length * 3) - 3
-        document.getElementById("newticket").innerHTML = tqnumber;
-        document.getElementById("people").innerHTML = people;
-        document.getElementById("waitingtime").innerHTML = wait_time;
-        break;
-      }
+    if (tqnumber === 299){
+      tqnumber = 100
+    }
+    else{
+      tqnumber += 1
+      taquilla_queu.push(tqnumber)
+      people = taquilla_queu.length - 1
+      wait_time = (taquilla_queu.length * 3) - 3
+      document.getElementById("newticket").innerHTML = tqnumber;
+      document.getElementById("people").innerHTML = people;
+      document.getElementById("waitingtime").innerHTML = wait_time;
+      break;
+    }
     case 2:
-      ttqnumber += 1
-      taquilla_tercera_queu.push(ttqnumber)
-      people = taquilla_tercera_queu.length
-      wait_time = (taquilla_tercera_queu.length * 5) - 5
-      document.getElementById("newticket").innerHTML = ttqnumber;
-      document.getElementById("people").innerHTML = people;
-      document.getElementById("waitingtime").innerHTML = wait_time;
-      break;
+    ttqnumber += 1
+    taquilla_tercera_queu.push(ttqnumber)
+    people = taquilla_tercera_queu.length - 1
+    wait_time = (taquilla_tercera_queu.length * 5) - 5
+    document.getElementById("newticket").innerHTML = ttqnumber;
+    document.getElementById("people").innerHTML = people;
+    document.getElementById("waitingtime").innerHTML = wait_time;
+    break;
     case 3:
-      acqnumber += 1
-      atencion_cliente_queu.push(acqnumber)
-      wait_time = (atencion_cliente_queu.length * 8) - 8
-      people = pop_atencion_client.length
-      console.log(atencion_cliente_queu)
-      document.getElementById("newticket").innerHTML = acqnumber;
-      document.getElementById("people").innerHTML = people;
-      document.getElementById("waitingtime").innerHTML = wait_time;
-      break;
+    acqnumber += 1
+    atencion_cliente_queu.push(acqnumber)
+    wait_time = (atencion_cliente_queu.length * 8) - 8
+    people = atencion_cliente_queu.length - 1
+    console.log(atencion_cliente_queu)
+    document.getElementById("newticket").innerHTML = acqnumber;
+    document.getElementById("people").innerHTML = people;
+    document.getElementById("waitingtime").innerHTML = wait_time;
+    break;
     case 4:
-      actqnumber += 1
-      atencion_cliente_tercera_queu.push(actqnumber)
-      wait_time = (atencion_cliente_tercera_queu.length * 12) - 12
-      people = atencion_cliente_tercera_queu.length
-      document.getElementById("newticket").innerHTML = actqnumber;
-      document.getElementById("people").innerHTML = people;
-      document.getElementById("waitingtime").innerHTML = wait_time;
-      break;
+    actqnumber += 1
+    atencion_cliente_tercera_queu.push(actqnumber)
+    wait_time = (atencion_cliente_tercera_queu.length * 12) - 12
+    people = atencion_cliente_tercera_queu.length - 1
+    document.getElementById("newticket").innerHTML = actqnumber;
+    document.getElementById("people").innerHTML = people;
+    document.getElementById("waitingtime").innerHTML = wait_time;
+    break;
     default:
-      break;
+    break;
   }
 
 }
 
+
 function pop_taquilla_client(){
+  document.getElementById("toattend1").innerHTML = taquilla_queu[0];
+  document.getElementById("ticketlist1").innerHTML = taquilla_queu.join('<br>');
   taquilla_queu.shift()
-  document.getElementById("toattend1").innerHTML = taquilla_queu.length;
-  document.getElementById("ticketlist1").innerHTML = taquilla_queu;
 
 }
 
 function pop_taquilla_tercera_client(){
-  taquilla_tercera_queu.shift()
-  document.getElementById("toattend2").innerHTML = taquilla_tercera_queu.length;
+  document.getElementById("toattend2").innerHTML = taquilla_tercera_queu[0];
   document.getElementById("ticketlist2").innerHTML = taquilla_tercera_queu;
+  taquilla_tercera_queu.shift()
+
 }
 
-
 function pop_atencion_client() {
-  atencion_cliente_queu.shift()
-  document.getElementById("toattend3").innerHTML = atencion_cliente_queu.length;
+  document.getElementById("toattend3").innerHTML = atencion_cliente_queu[0];
   document.getElementById("ticketlist3").innerHTML = atencion_cliente_queu;
+  atencion_cliente_queu.shift()
 }
 
 function pop_atencion_tercera_client(){
-  atencion_cliente_tercera_queu.shift()
-  document.getElementById("toattend4").innerHTML = atencion_cliente_tercera_queu.length;
+  document.getElementById("toattend4").innerHTML = atencion_cliente_tercera_queu[0];
   document.getElementById("ticketlist4").innerHTML = atencion_cliente_tercera_queu;
+  atencion_cliente_tercera_queu.shift()
 
 }
 
